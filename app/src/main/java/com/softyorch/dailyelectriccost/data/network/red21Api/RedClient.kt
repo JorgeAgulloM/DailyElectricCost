831f3ca885cacabb3727c58718bc14e8768dd5c9
Jorge Agulló <agullojorge@gmail.com>
2022-11-05 14:23:51 +0100

    [WIP] Client Red21 added

---
+++ app/src/main/java/com/softyorch/dailyelectriccost/data/network/red21Api/RedClient.kt
@@ -0,0 +1,16 @@
+package com.softyorch.dailyelectriccost.data.network.red21Api
+
+import com.softyorch.dailyelectriccost.data.network.red21Api.response.Red21
+import retrofit2.Response
+import retrofit2.http.GET
+import javax.inject.Singleton
+
+@Singleton
+interface RedClient {
+
+    //url e.g. https://apidatos.ree.es/es/datos/balance/balance-electrico?start_date=2019-01-01T00:00&end_date=2019-01-31T23:59&time_trunc=day
+
+    @GET("es/datos")
+    suspend fun getData(): Response<Red21>
+
+}
