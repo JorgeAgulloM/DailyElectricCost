46ff0e7d55a33cdf6d41132842b8e651112d1dcc
Jorge Agulló <agullojorge@gmail.com>
2022-11-07 16:47:56 +0100

    [WIP] Navigation and screen added

--- app/src/main/java/com/softyorch/dailyelectriccost/ui/activities/MainActivity.kt
+++ app/src/main/java/com/softyorch/dailyelectriccost/ui/activities/MainActivity.kt
@@ -3,50 +3,20 @@ package com.softyorch.dailyelectriccost.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.softyorch.dailyelectriccost.ui.theme.DailyElectricCostTheme
+import com.softyorch.dailyelectriccost.ui.screens.DailyElectricCost
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity: ComponentActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val viewModel = hiltViewModel<MainViewModel>()
             viewModel.getDataDefault()
             viewModel.getDataGeoTruncate()
-            //viewModel.getDataPrueba()
-            DailyElectricCostTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
+            DailyElectricCost()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DailyElectricCostTheme {
-        Greeting("Android")
     }
 }
