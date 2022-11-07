9166d94187350e2474cb2bebe7880d3b69154f52
Jorge Agulló <agullojorge@gmail.com>
2022-11-07 08:39:39 +0100

    [WIP] The mappers has been added

---
+++ app/src/main/java/com/softyorch/dailyelectriccost/data/repository/red21Repository/model/mapper/MapToEntity.kt
@@ -0,0 +1,14 @@
+package com.softyorch.dailyelectriccost.data.repository.red21Repository.model.mapper
+
+import com.softyorch.dailyelectriccost.data.network.red21Api.entity.RedDataTruncateEntity
+import com.softyorch.dailyelectriccost.data.network.red21Api.entity.RedDefaultEntity
+import com.softyorch.dailyelectriccost.data.repository.red21Repository.model.RedDataTruncateModel
+import com.softyorch.dailyelectriccost.data.repository.red21Repository.model.RedDefaultModel
+
+fun RedDefaultModel.mapToRedDefaultEntity(): RedDefaultEntity = RedDefaultEntity(
+    endDate, startDate, endDate, timeTruncate
+)
+
+fun RedDataTruncateModel.mapToRedDataTruncateEntity(): RedDataTruncateEntity = RedDataTruncateEntity(
+    widget, startDate, endDate, timeTruncate, geo_limit, geo_ids
+)
