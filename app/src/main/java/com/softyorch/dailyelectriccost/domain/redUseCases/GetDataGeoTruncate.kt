59c645b6247ef01e957a6c8795bd2894fae39a60
Jorge Agulló <agullojorge@gmail.com>
2022-11-07 08:50:01 +0100

    [WIP] name changed

--- app/src/main/java/com/softyorch/dailyelectriccost/domain/redUseCases/GetDataGeoTruncate.kt
+++ app/src/main/java/com/softyorch/dailyelectriccost/domain/redUseCases/GetDataGeoTruncate.kt
@@ -5,7 +5,7 @@ import com.softyorch.dailyelectriccost.data.repository.red21Repository.model.map
 import com.softyorch.dailyelectriccost.domain.redUseCases.model.RedDataTruncateModelDomain
 import javax.inject.Inject
 
-class GetDataGeoTruncateUseCases @Inject constructor(private val repository: Red21Repository) {
+class GetDataGeoTruncate @Inject constructor(private val repository: Red21Repository) {
     suspend operator fun invoke(redDataTruncateModelDomain: RedDataTruncateModelDomain) =
         repository.getDataGeoTruncate(redDataTruncateModelDomain.mapToRedDataTruncateModel())
 }
