47f71f87b47c6ca94a5d1f6ad6a83264d45a0566
Jorge Agulló <agullojorge@gmail.com>
2022-11-07 16:57:38 +0100

    [WIP] Added needed functions from MainActivity

--- app/src/main/java/com/softyorch/dailyelectriccost/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/softyorch/dailyelectriccost/ui/screens/main/MainScreen.kt
@@ -1,9 +1,33 @@
 package com.softyorch.dailyelectriccost.ui.screens.main
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen(navController: NavController) {
+fun MainScreen(navController: NavController, viewModel: MainViewModel) {
 
+    Scaffold(
+
+
+    ) {
+        Column(
+            modifier = Modifier.padding(it.calculateTopPadding()).fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Hola, soy Main")
+        }
+    }
 }
--- app/src/main/java/com/softyorch/dailyelectriccost/ui/screens/main/MainViewModel.kt
+++ app/src/main/java/com/softyorch/dailyelectriccost/ui/screens/main/MainViewModel.kt
@@ -1,11 +1,66 @@
 package com.softyorch.dailyelectriccost.ui.screens.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.softyorch.dailyelectriccost.domain.redUseCases.RedUsesCases
+import com.softyorch.dailyelectriccost.domain.redUseCases.model.mapper.mapToRedDataTruncateModelDomain
+import com.softyorch.dailyelectriccost.domain.redUseCases.model.mapper.mapToRedDefaultModelDomain
+import com.softyorch.dailyelectriccost.ui.model.RedDataTruncateModelUi
+import com.softyorch.dailyelectriccost.ui.model.RedDefaultModelUi
+import com.softyorch.dailyelectriccost.utils.Constants
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(private val redUsesCases: RedUsesCases) : ViewModel() {
+
+    fun getDataDefault() {
+        viewModelScope.launch {
+            val response = redUsesCases.getDataDefault(
+                RedDefaultModelUi(
+                    category = Constants.LIST_CATEGORY[0][0],
+                    widget = Constants.LIST_CATEGORY[0][1],
+                    startDate = "2019-01-01T00:00",
+                    endDate = "2019-01-31T23:59",
+                    timeTruncate = "day"
+                ).mapToRedDefaultModelDomain()
+            )
+            response.let {
+                if (it != null) {
+                    if (it.isSuccessful) {
+                        Log.d(Constants.RED21, "dataVM.default ->${it.body()}")
+                    } else {
+                        Log.d(Constants.RED21, "dataVM.default.error ->${it.errorBody()}")
+                    }
+                }
+            }
+        }
+    }
+    fun getDataGeoTruncate() {
+        viewModelScope.launch {
+            val response = redUsesCases.getDataGeoTruncate(
+                RedDataTruncateModelUi(
+                    category = Constants.LIST_CATEGORY[0][0],
+                    widget = Constants.LIST_CATEGORY[0][1],
+                    startDate = "2019-01-01T00:00",
+                    endDate = "2019-01-31T23:59",
+                    timeTruncate = "day",
+                    geo_limit = "peninsular",
+                    geo_ids = "8741"
+                ).mapToRedDataTruncateModelDomain()
+            )
+            response.let {
+                if (it != null) {
+                    if (it.isSuccessful) {
+                        Log.d(Constants.RED21, "dataVM.geo ->${it.body()}")
+                    } else {
+                        Log.d(Constants.RED21, "dataVM.geo.error ->${it.errorBody()}")
+                    }
+                }
+            }
+        }
+    }
 
 }
