87b065db860247e907a983c09efe571cbda9cb25
Jorge Agulló <agullojorge@gmail.com>
2022-11-07 08:39:56 +0100

    [WIP] The UI models has been added

---
+++ app/src/main/java/com/softyorch/dailyelectriccost/model/RedDefaultModelUi.kt
@@ -0,0 +1,8 @@
+package com.softyorch.dailyelectriccost.model
+
+data class RedDefaultModelUi(
+    val widget: String,
+    val startDate: String,
+    val endDate: String,
+    val timeTruncate: String
+)
