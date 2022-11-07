2e394c51601be892ab59ee664fbab8d3d4d50a1b
Jorge Agulló <agullojorge@gmail.com>
2022-11-07 16:57:04 +0100

    [WIP] Deleted non needed functions

--- app/src/main/java/com/softyorch/dailyelectriccost/ui/activities/MainActivity.kt
+++ app/src/main/java/com/softyorch/dailyelectriccost/ui/activities/MainActivity.kt
@@ -3,7 +3,6 @@ package com.softyorch.dailyelectriccost.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.softyorch.dailyelectriccost.ui.screens.DailyElectricCost
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,9 +11,7 @@ class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel = hiltViewModel<MainViewModel>()
-            viewModel.getDataDefault()
-            viewModel.getDataGeoTruncate()
+            //val viewModel = hiltViewModel<MainViewModel>()
 
             DailyElectricCost()
         }
