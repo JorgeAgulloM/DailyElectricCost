504bc1460e3b6ed8943798c282bbce4f602b6e0a
Jorge Agulló <agullojorge@gmail.com>
2022-11-07 19:23:17 +0100

    [WIP] New manual test

--- app/src/main/java/com/softyorch/dailyelectriccost/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/softyorch/dailyelectriccost/ui/screens/main/MainScreen.kt
@@ -9,9 +9,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.softyorch.dailyelectriccost.data.network.red21Api.responseMercados.Red21Mercados
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -19,15 +22,23 @@ import androidx.navigation.NavController
 fun MainScreen(navController: NavController, viewModel: MainViewModel) {
 
     Scaffold(
-
-
     ) {
+
+        val dataDefault: Red21Mercados? by viewModel.defaultData.observeAsState(initial = Red21Mercados.red21MercadosEmpty)
+
         Column(
             modifier = Modifier.padding(it.calculateTopPadding()).fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceEvenly,
+            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Hola, soy Main")
+            Text(text = dataDefault?.included?.get(0)?.attributes?.title ?: "null")
+            Text(text = dataDefault?.included?.get(0)?.attributes?.description?.toString() ?: "null")
+            Text(text = dataDefault?.included?.get(0)?.attributes?.magnitude ?: "null")
+            Text(text = dataDefault?.included?.get(0)?.attributes?.lastUpdate ?: "null")
+            Text(text = dataDefault?.included?.get(0)?.attributes?.type?.toString() ?: "null")
+            Text(text = dataDefault?.included?.get(0)?.attributes?.composite?.toString() ?: "null")
+            Text(text = dataDefault?.included?.get(0)?.attributes?.color ?: "null")
+            Text(text = dataDefault?.included?.get(0)?.attributes?.values?.get(0)?.value?.toString() ?: "null")
         }
     }
 }
--- app/src/main/java/com/softyorch/dailyelectriccost/ui/screens/main/MainViewModel.kt
+++ app/src/main/java/com/softyorch/dailyelectriccost/ui/screens/main/MainViewModel.kt
@@ -1,8 +1,11 @@
 package com.softyorch.dailyelectriccost.ui.screens.main
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.softyorch.dailyelectriccost.data.network.red21Api.responseMercados.Red21Mercados
 import com.softyorch.dailyelectriccost.domain.redUseCases.RedUsesCases
 import com.softyorch.dailyelectriccost.domain.redUseCases.model.mapper.mapToRedDataTruncateModelDomain
 import com.softyorch.dailyelectriccost.domain.redUseCases.model.mapper.mapToRedDefaultModelDomain
@@ -16,20 +19,29 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(private val redUsesCases: RedUsesCases) : ViewModel() {
 
+    private val _defaultData = MutableLiveData<Red21Mercados?>(Red21Mercados.red21MercadosEmpty)
+    val defaultData: LiveData<Red21Mercados?> = _defaultData
+
+    init {
+        //getDataDefault()
+        getDataGeoTruncate()
+    }
+
     fun getDataDefault() {
         viewModelScope.launch {
             val response = redUsesCases.getDataDefault(
                 RedDefaultModelUi(
-                    category = Constants.LIST_CATEGORY[0][0],
-                    widget = Constants.LIST_CATEGORY[0][1],
-                    startDate = "2019-01-01T00:00",
-                    endDate = "2019-01-31T23:59",
-                    timeTruncate = "day"
+                    category = Constants.LIST_CATEGORY[5][0],
+                    widget = Constants.LIST_CATEGORY[5][13],
+                    startDate = "2022-11-07T17:00",
+                    endDate = "2022-11-07T23:00",
+                    timeTruncate = "hour"
                 ).mapToRedDefaultModelDomain()
             )
             response.let {
                 if (it != null) {
                     if (it.isSuccessful) {
+                        _defaultData.postValue(it.body())
                         Log.d(Constants.RED21, "dataVM.default ->${it.body()}")
                     } else {
                         Log.d(Constants.RED21, "dataVM.default.error ->${it.errorBody()}")
@@ -38,15 +50,16 @@ class MainViewModel @Inject constructor(private val redUsesCases: RedUsesCases) 
             }
         }
     }
+
     fun getDataGeoTruncate() {
         viewModelScope.launch {
             val response = redUsesCases.getDataGeoTruncate(
                 RedDataTruncateModelUi(
-                    category = Constants.LIST_CATEGORY[0][0],
-                    widget = Constants.LIST_CATEGORY[0][1],
-                    startDate = "2019-01-01T00:00",
-                    endDate = "2019-01-31T23:59",
-                    timeTruncate = "day",
+                    category = Constants.LIST_CATEGORY[5][0],
+                    widget = Constants.LIST_CATEGORY[5][13],
+                    startDate = "2022-11-07T17:00",
+                    endDate = "2022-11-07T18:00",
+                    timeTruncate = "hour",
                     geo_limit = "peninsular",
                     geo_ids = "8741"
                 ).mapToRedDataTruncateModelDomain()
@@ -54,6 +67,7 @@ class MainViewModel @Inject constructor(private val redUsesCases: RedUsesCases) 
             response.let {
                 if (it != null) {
                     if (it.isSuccessful) {
+                        _defaultData.postValue(it.body())
                         Log.d(Constants.RED21, "dataVM.geo ->${it.body()}")
                     } else {
                         Log.d(Constants.RED21, "dataVM.geo.error ->${it.errorBody()}")
