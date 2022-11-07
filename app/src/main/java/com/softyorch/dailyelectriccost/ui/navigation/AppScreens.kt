48595364e855b24df8ad46d1a8f6391445f2ea3f
Jorge Agulló <agullojorge@gmail.com>
2022-11-07 16:46:48 +0100

    [WIP] Navigation added

---
+++ app/src/main/java/com/softyorch/dailyelectriccost/ui/navigation/AppScreens.kt
@@ -0,0 +1,6 @@
+package com.softyorch.dailyelectriccost.ui.navigation
+
+enum class AppScreens {
+    SplashScreen,
+    MainScreen
+}
---
+++ app/src/main/java/com/softyorch/dailyelectriccost/ui/screens/splash/SplashScreen.kt
@@ -0,0 +1,9 @@
+package com.softyorch.dailyelectriccost.ui.screens.splash
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SplashScreen(navController: NavController) {
+
+}
