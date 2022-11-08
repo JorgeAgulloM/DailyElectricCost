47e6d018c2e394ad58ac4626e98ffaf0994f69a5
Jorge Agulló <agullojorge@gmail.com>
2022-11-08 14:24:04 +0100

    [WIP] Delete old response

--- app/src/main/java/com/softyorch/dailyelectriccost/data/network/red21Api/responseMercados/Meta.kt
+++
@@ -1,7 +0,0 @@
-package com.softyorch.dailyelectriccost.data.network.red21Api.responseMercados
-
-import com.google.gson.annotations.SerializedName
-
-data class Meta(
-    @SerializedName("cache-control") val cacheControl: CacheControl
-)
