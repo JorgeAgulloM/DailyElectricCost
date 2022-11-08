7293de2de8e1b765e54df557fb38ba4753902418
Jorge Agulló <agullojorge@gmail.com>
2022-11-08 13:50:01 +0100

    [WIP] Manual Test

--- app/src/main/java/com/softyorch/dailyelectriccost/ui/screens/main/MainViewModel.kt
+++ app/src/main/java/com/softyorch/dailyelectriccost/ui/screens/main/MainViewModel.kt
@@ -5,16 +5,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.softyorch.dailyelectriccost.data.network.red21Api.responseMercados.Included
 import com.softyorch.dailyelectriccost.data.network.red21Api.responseMercados.Red21Response
 import com.softyorch.dailyelectriccost.domain.redUseCases.RedUsesCases
 import com.softyorch.dailyelectriccost.domain.redUseCases.model.mapper.mapToRedDataTruncateModelDomain
-import com.softyorch.dailyelectriccost.domain.redUseCases.model.mapper.mapToRedDefaultModelDomain
 import com.softyorch.dailyelectriccost.ui.model.RedDataTruncateModelUi
-import com.softyorch.dailyelectriccost.ui.model.RedDefaultModelUi
 import com.softyorch.dailyelectriccost.utils.Constants
+import com.softyorch.dailyelectriccost.utils.Constants.EMPTY_STRING
+import com.softyorch.dailyelectriccost.utils.funcExtensions.toDateFormatted
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
 
 @HiltViewModel
 class MainViewModel @Inject constructor(private val redUsesCases: RedUsesCases) : ViewModel() {
@@ -22,12 +24,41 @@ class MainViewModel @Inject constructor(private val redUsesCases: RedUsesCases) 
     private val _defaultData = MutableLiveData<Red21Response?>(Red21Response.red21ResponseEmpty)
     val defaultData: LiveData<Red21Response?> = _defaultData
 
+    private val _includes =
+        MutableLiveData<List<Included?>>(Red21Response.red21ResponseEmpty.included)
+    val includes: LiveData<List<Included?>> = _includes
+
+
+    private val _marketType = MutableLiveData<String>(EMPTY_STRING)
+    val marketType: LiveData<String> = _marketType
+
+    private val _lastUpdate = MutableLiveData<String>(EMPTY_STRING)
+    val lastUpdate: LiveData<String> = _lastUpdate
+
+    private val _price = MutableLiveData<Double>(0.0)
+    val price: LiveData<Double> = _price
+
+    private val _dateTime = MutableLiveData<String>(EMPTY_STRING)
+    val dateTime: LiveData<String> = _dateTime
+
+    /** Query data **********************************************/
+
+    private val _category = Constants.LIST_CATEGORY
+    private val _startDate = MutableLiveData("2022-11-07T17:00")
+    private val _endDate = MutableLiveData("2022-11-07T18:00")
+    private val _timeTruncate = MutableLiveData("hour")
+    private val _geoLimit = MutableLiveData("peninsular")
+    val geoLimit: LiveData<String> = _geoLimit
+
+    private val _geoIds = MutableLiveData("8741")
+
+    private val oneHourInMillis = 3600000L
     init {
         //getDataDefault()
         getDataGeoTruncate()
     }
 
-    fun getDataDefault() {
+    /*fun getDataDefault() {
         viewModelScope.launch {
             val response = redUsesCases.getDataDefault(
                 RedDefaultModelUi(
@@ -49,28 +80,44 @@ class MainViewModel @Inject constructor(private val redUsesCases: RedUsesCases) 
                 }
             }
         }
-    }
+    }*/
+
 
     fun getDataGeoTruncate() {
+
         viewModelScope.launch {
-            val response = redUsesCases.getDataGeoTruncate(
+            redUsesCases.getDataGeoTruncate(
                 RedDataTruncateModelUi(
-                    category = Constants.LIST_CATEGORY[5][0],
-                    widget = Constants.LIST_CATEGORY[5][13],
-                    startDate = "2022-11-07T17:00",
-                    endDate = "2022-11-07T18:00",
-                    timeTruncate = "hour",
-                    geo_limit = "peninsular",
-                    geo_ids = "8741"
-                ).mapToRedDataTruncateModelDomain()
-            )
-            response.let {
-                if (it != null) {
-                    if (it.isSuccessful) {
-                        _defaultData.postValue(it.body())
-                        Log.d(Constants.RED21, "dataVM.geo ->${it.body()}")
+                    category = _category[5][0],
+                    widget = _category[5][13],
+                    startDate = _startDate.value!!,
+                    endDate = _endDate.value!!,/*if (sdk26AndUp) {
+                        (Date.from(Instant.now())).toString()
                     } else {
-                        Log.d(Constants.RED21, "dataVM.geo.error ->${it.errorBody()}")
+                        LocalDateTime.now().toString()
+                    },//_endDate.value!!,*/
+                    timeTruncate = _timeTruncate.value!!,
+                    geo_limit = _geoLimit.value!!,
+                    geo_ids = _geoIds.value!!
+                ).mapToRedDataTruncateModelDomain()
+            ).let { response ->
+                if (response != null) {
+                    if (response.isSuccessful) {
+                        _defaultData.postValue(response.body())
+                        _includes.postValue(response.body()!!.included)
+
+                        response.body()?.included?.get(0)?.attributes.let { attr ->
+                            _marketType.postValue(attr?.title)
+                            _lastUpdate.postValue(attr?.lastUpdate?.toDateFormatted() ?: "Desconocido")
+                            attr?.values?.get(1).let { values ->
+                                _price.postValue(values?.value ?: 0.0)
+                                _dateTime.postValue(values?.datetime?.toDateFormatted() ?: "Desconocido")
+                            }
+                        }
+
+                        Log.d(Constants.RED21, "dataVM.geo ->${response.body()}")
+                    } else {
+                        Log.d(Constants.RED21, "dataVM.geo.error ->${response.errorBody()}")
                     }
                 }
             }
