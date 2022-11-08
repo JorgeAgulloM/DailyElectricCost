d250e1ec22f3d1cc8aad356c0bf3be40ee0f5ffb
Jorge Agulló <agullojorge@gmail.com>
2022-11-08 17:46:23 +0100

    [WIP] New function for new service added

--- app/src/main/java/com/softyorch/dailyelectriccost/data/network/red21Api/RedService.kt
+++ app/src/main/java/com/softyorch/dailyelectriccost/data/network/red21Api/RedService.kt
@@ -3,6 +3,8 @@ package com.softyorch.dailyelectriccost.data.network.red21Api
 import android.util.Log
 import com.softyorch.dailyelectriccost.data.network.red21Api.entity.RedDataTruncateEntity
 import com.softyorch.dailyelectriccost.data.network.red21Api.entity.RedDefaultEntity
+import com.softyorch.dailyelectriccost.data.network.red21Api.response.balance.Red21Balance
+import com.softyorch.dailyelectriccost.data.network.red21Api.response.generation.Red21Generation
 import com.softyorch.dailyelectriccost.data.network.red21Api.response.market.Red21Market
 import com.softyorch.dailyelectriccost.utils.Constants.RED21
 import kotlinx.coroutines.Dispatchers
@@ -14,7 +16,7 @@ class RedService @Inject constructor(private val redClient: RedClient) {
     /** Asimilación de datos del balance eléctrico durante las últimas 24 horas. */
     suspend fun getDataDefault(
         redDefaultEntity: RedDefaultEntity
-    ): Response<Red21Market>? {
+    ): Response<Red21Balance>? {
         return withContext(Dispatchers.IO) {
             try {
                 val response = redClient.getDataDefault(
@@ -24,21 +26,44 @@ class RedService @Inject constructor(private val redClient: RedClient) {
                     endDate = redDefaultEntity.endDate,
                     timeTruncate = redDefaultEntity.timeTruncate
                 )
-                Log.d(RED21, "ResponseDefault.Service ->$response")
+                Log.d(RED21, "Response.getDataDefault.Service ->$response")
                 response
             } catch (ex: java.lang.Exception) {
-                Log.d(RED21, "ResponseDefault.Service.error ->${ex.message.toString()}")
+                Log.d(RED21, "Response.getDataDefault.Service.error ->${ex.message.toString()}")
                 null
             }
         }
     }
 
-    suspend fun getDataGeoTruncate(
+    /** Asimilación de datos de la generación de energía del último dato guardado. */
+    suspend fun getGenerationDefault(
+        redDefaultEntity: RedDefaultEntity
+    ): Response<Red21Generation>? {
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = redClient.getGenerationGeoTruncate(
+                    category = redDefaultEntity.category,
+                    widget = redDefaultEntity.widget,
+                    startDate = redDefaultEntity.startDate,
+                    endDate = redDefaultEntity.endDate,
+                    timeTruncate = redDefaultEntity.timeTruncate
+                )
+                Log.d(RED21, "Response.getGenerationDefault.Service ->$response")
+                response
+            } catch (ex: java.lang.Exception) {
+                Log.d(RED21, "Response.getGenerationDefault.Service.error ->${ex.message.toString()}")
+                null
+            }
+        }
+    }
+
+    /** Asimilación de datos de los mercados en tiempo real, actualizado cada hora. */
+    suspend fun getMarketsGeoTruncate(
         redDataTruncateEntity: RedDataTruncateEntity
     ): Response<Red21Market>? {
         return withContext(Dispatchers.IO) {
             try {
-                val response = redClient.getDataGeoTruncate(
+                val response = redClient.getMarketsGeoTruncate(
                     category = redDataTruncateEntity.category,
                     widget = redDataTruncateEntity.widget,
                     startDate = redDataTruncateEntity.startDate,
@@ -47,10 +72,10 @@ class RedService @Inject constructor(private val redClient: RedClient) {
                     geo_limit = redDataTruncateEntity.geo_limit,
                     geo_ids = redDataTruncateEntity.geo_ids
                 )
-                Log.d(RED21, "ResponseDataTruncate.Service ->$response")
+                Log.d(RED21, "Response.getMarketsGeoTruncate.Service ->$response")
                 response
             } catch (ex: java.lang.Exception) {
-                Log.d(RED21, "ResponseDataTruncate.Service.error ->${ex.message.toString()}")
+                Log.d(RED21, "Response.getMarketsGeoTruncate.Service.error ->${ex.message.toString()}")
                 null
             }
         }
