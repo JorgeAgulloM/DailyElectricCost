b5926a073580b23a1a91eab9496292a91651b795
Jorge Agulló <agullojorge@gmail.com>
2022-11-08 17:59:21 +0100

    [WIP] New model added.

---
+++ app/src/main/java/com/softyorch/dailyelectriccost/domain/redUseCases/model/RedGenerationTruncateModel.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022. File developed for DailyElectricCost App by Jorge Agulló Martín for SoftYorch
+ */
+
+package com.softyorch.dailyelectriccost.domain.redUseCases.model
+
+data class RedGenerationTruncateModel(
+    val category: String,
+    var widget: String,
+    var startDate: String,
+    var endDate: String,
+    var timeTruncate: String,
+    var geo_limit: String,
+    var geo_ids: String
+)
