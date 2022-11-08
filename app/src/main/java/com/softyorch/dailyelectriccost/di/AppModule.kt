c59a18600ce54f238df95307b0c48c7e11b4ff4a
Jorge Agulló <agullojorge@gmail.com>
2022-11-08 18:14:53 +0100

    [WIP] New use case added.

--- app/src/main/java/com/softyorch/dailyelectriccost/di/AppModule.kt
+++ app/src/main/java/com/softyorch/dailyelectriccost/di/AppModule.kt
@@ -2,6 +2,7 @@ package com.softyorch.dailyelectriccost.di
 
 import com.softyorch.dailyelectriccost.data.repository.red21Repository.Red21Repository
 import com.softyorch.dailyelectriccost.domain.redUseCases.GetDataBalance
+import com.softyorch.dailyelectriccost.domain.redUseCases.GetDataGenerationTruncate
 import com.softyorch.dailyelectriccost.domain.redUseCases.GetDataMarketsTruncate
 import com.softyorch.dailyelectriccost.domain.redUseCases.RedUsesCases
 import dagger.Module
@@ -19,6 +20,7 @@ object AppModule {
     fun providesRedUsesCases(repository: Red21Repository): RedUsesCases =
         RedUsesCases(
             getDataBalance = GetDataBalance(repository),
+            getDataGenerationTruncate = GetDataGenerationTruncate(repository),
             getDataMarketsTruncate = GetDataMarketsTruncate(repository)
         )
 }
--- app/src/main/java/com/softyorch/dailyelectriccost/domain/redUseCases/RedUsesCases.kt
+++ app/src/main/java/com/softyorch/dailyelectriccost/domain/redUseCases/RedUsesCases.kt
@@ -2,5 +2,6 @@ package com.softyorch.dailyelectriccost.domain.redUseCases
 
 data class RedUsesCases(
     val getDataBalance: GetDataBalance,
+    val getDataGenerationTruncate: GetDataGenerationTruncate,
     val getDataMarketsTruncate: GetDataMarketsTruncate
 )
