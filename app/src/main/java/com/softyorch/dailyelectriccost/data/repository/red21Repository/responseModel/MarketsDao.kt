cfb74e0df0b4a00fe53d5e602864e2ac4c7b0c92
Jorge Agulló <agullojorge@gmail.com>
2022-11-09 05:47:31 +0100

    [WIP] MarketsDao has been added

---
+++ app/src/main/java/com/softyorch/dailyelectriccost/data/repository/red21Repository/responseModel/MarketsDao.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022. File developed for DailyElectricCost App by Jorge Agulló Martín for SoftYorch
+ */
+
+package com.softyorch.dailyelectriccost.data.repository.red21Repository.responseModel
+
+import com.softyorch.dailyelectriccost.utils.Constants.EMPTY_STRING
+
+data class MarketsDao(
+    var title: String,
+    var lastUpdate: String,
+    var type: String,
+    var lowPrice: Double,
+    var hiPrice: Double,
+    var currentPrice: Double,
+    var values: MutableList<Values>
+){
+    companion object{
+        val emptyMarketsDao: MarketsDao = MarketsDao(
+            title = EMPTY_STRING,
+            lastUpdate = EMPTY_STRING,
+            type = EMPTY_STRING,
+            lowPrice = 0.0,
+            hiPrice = 0.0,
+            currentPrice = 0.0,
+            values = mutableListOf(
+                Values(
+                    value = 0.0,
+                    dateTime = EMPTY_STRING
+                )
+            )
+        )
+    }
+}
