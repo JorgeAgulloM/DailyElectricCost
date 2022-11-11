7a7cf4b489f3e029e520bb6d11968d24303cc8a1
Jorge Agulló <agullojorge@gmail.com>
2022-11-11 19:49:06 +0100

    [WIP] Refactor and set new values

--- app/src/main/java/com/softyorch/dailyelectriccost/data/repository/red21Repository/IRed21Repository.kt
+++ app/src/main/java/com/softyorch/dailyelectriccost/data/repository/red21Repository/IRed21Repository.kt
@@ -6,15 +6,18 @@ package com.softyorch.dailyelectriccost.data.repository.red21Repository
 
 import android.util.Log
 import com.softyorch.dailyelectriccost.data.network.red21Api.response.market.Red21Market
+import com.softyorch.dailyelectriccost.data.network.red21Api.response.market.Value
 import com.softyorch.dailyelectriccost.data.repository.red21Repository.dao.MarketsDao
 import com.softyorch.dailyelectriccost.data.repository.red21Repository.dao.Values
-import com.softyorch.dailyelectriccost.utils.Constants
+import com.softyorch.dailyelectriccost.utils.Constants.RED21
 import com.softyorch.dailyelectriccost.utils.funcExtensions.getHourOfCalendarToInt
+import com.softyorch.dailyelectriccost.utils.funcExtensions.getHourOfDate
 import com.softyorch.dailyelectriccost.utils.funcExtensions.getHourOfNowToInt
 import retrofit2.Response
 import java.util.*
+import kotlin.collections.ArrayList
 
-interface IRed21Repository{
+interface IRed21Repository {
     fun loadDao(responseApi: Response<Red21Market>?): MarketsDao {
         val dao = MarketsDao.emptyMarketsDao
         responseApi.let { response ->
@@ -22,43 +25,98 @@ interface IRed21Repository{
                 if (response.isSuccessful) {
 
                     response.body()!!.let { body ->
-                        body.data.attributes.let { data ->
-                            dao.title = data.title
-                            dao.lastUpdate = data.lastUpdate
-                        }
-                        body.included[0].let { included ->
-                            dao.type = included.type
-                            var lowPrice = 99999999.0
-                            var hiPrice = 0.0
-                            var currentPrices = 0.0
-                            var loadAverage = 0.0
-                            var divideAverage = 0.0
-                            val nowHour =
-                                Calendar.getInstance().time.toString().getHourOfCalendarToInt()
-                            included.attributes.values.forEach { value ->
-                                if (value.value > 0.0) {
-                                    loadAverage += value.value
-                                    divideAverage++
-                                }
-                                if (value.value > hiPrice) hiPrice = value.value
-                                if (value.value < lowPrice) lowPrice = value.value
-                                if (value.datetime.getHourOfNowToInt() == nowHour)
-                                    currentPrices = value.value
-                                dao.values.add(Values(value.value, value.datetime))
-                            }
-                            dao.lowPrice = lowPrice
-                            dao.hiPrice = hiPrice
-                            dao.currentPrice = currentPrices
-                            dao.avgPrice = loadAverage / divideAverage
-                        }
+                        getData(body, dao)
+                        getIncluded(body, dao)
                     }
                 } else {
-                    Log.d(Constants.RED21, "getDataMarkets.error ->${response.errorBody()}")
+                    Log.d(RED21, "getDataMarkets.error ->${response.errorBody()}")
                 }
             } else {
-                Log.d(Constants.RED21, "getDataMarkets response is null")
+                Log.d(RED21, "getDataMarkets response is null")
             }
         }
         return dao
     }
+
+    private fun getData(
+        body: Red21Market,
+        dao: MarketsDao
+    ) {
+        body.data.attributes.let { data ->
+            dao.title = data.title
+            dao.lastUpdate = data.lastUpdate
+        }
+    }
+
+    private fun getIncluded(
+        body: Red21Market,
+        dao: MarketsDao
+    ) {
+        body.included[0].let { included ->
+            dao.type = included.type
+
+
+            var currentPrices = 0.0
+            var loadAverage = 0.0
+            var divideAverage = 0.0
+            val nowHour =
+                Calendar.getInstance().time.toString().getHourOfCalendarToInt()
+
+            included.attributes.values.let {
+                dao.apply {
+                    it.forEach { value ->
+                        if (value.value > 0.0) {
+                            loadAverage += value.value
+                            divideAverage++
+                        }
+                        if (value.datetime.getHourOfNowToInt() == nowHour) currentPrices =
+                            value.value
+                        values.add(Values(value.value, value.datetime))
+                    }
+                    currentPrice = currentPrices
+                    avgPrice = loadAverage / divideAverage
+                    bestLowRange = bestHoursRange(it)
+                    lowPrice = it.minOf { value -> value.value }
+                    hiPrice = it.maxOf { value -> value.value }
+                    lowHour = it.findLast { value -> value.value == dao.lowPrice }!!.datetime.getHourOfDate()
+                    hiHour = it.findLast { value -> value.value == dao.hiPrice }!!.datetime.getHourOfDate()
+                }
+            }
+        }
+    }
+
+    private fun MarketsDao.bestHoursRange(it: List<Value>): String {
+        val listOfHoursLowPrice = it.filter { value -> value.value <= avgPrice }
+
+        var lastHourChecked = 0
+        val blockListHours = mutableListOf<Int>()
+        val listOfBlockListHours = mutableListOf<MutableList<Int>>()
+        listOfHoursLowPrice.forEach { value ->
+            lastHourChecked = if (lastHourChecked == 0) value.datetime.getHourOfNowToInt()
+            else {
+                val now = value.datetime.getHourOfNowToInt()
+                blockListHours.add(lastHourChecked)
+                if (lastHourChecked + 1 != now) {
+                    listOfBlockListHours.add(ArrayList(blockListHours))
+                    blockListHours.clear()
+                }
+                now
+            }
+        }
+        blockListHours.add(lastHourChecked)
+        listOfBlockListHours.add(ArrayList(blockListHours))
+
+        //
+        blockListHours.clear()
+        listOfBlockListHours.sortByDescending { newList -> newList.size }
+        blockListHours.addAll(listOfBlockListHours[0])
+
+        return "El rango de horas más económico es de la ${
+            blockListHours.first()
+        } am hasta las ${
+            blockListHours.last()
+        } pm"
+
+    }
+
 }
